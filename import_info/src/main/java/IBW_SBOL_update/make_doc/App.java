538c1eca83cb128e0a887b784c70987c886f43e3
BoyDun <bodun@stanford.edu>
2017-06-18 13:09:55 -0400

    add comments

--- import_info/src/main/java/IBW_SBOL_update/make_doc/App.java
+++ import_info/src/main/java/IBW_SBOL_update/make_doc/App.java
@@ -13,16 +13,23 @@ public class App
 {
 	
     public static void main( String[] args ) {
+    	
+    	//Chose an arbitrary, SBOL-compliant part to extract its DNA sequence from.
     	String partName = "BO_28543";
+    	
     	System.out.println(getSequenceFromNCL(partName));
     }
 
+    /**
+     * Returns a DNA sequence in string form from a specified SBOL part
+     * @param partName
+     * @return
+     */
     private static String getSequenceFromNCL(String partName) {
 
     	String sequence = "";
     	try {
     		InputStream url = new URL("http://sbol.ncl.ac.uk:8081/part/" + partName + "/sbol").openStream();
-    		
     		SBOLDocument sbol = SBOLReader.read(url);
     		Set<Sequence> sequences = sbol.getSequences();
     		sequence = sequences.iterator().next().getElements();
